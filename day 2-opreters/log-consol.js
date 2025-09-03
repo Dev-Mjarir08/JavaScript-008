e318eeebe845b0b9d74e568f8d8440c60808c3a1
Dev-Mjarir08 <08mjarir@gmail.com>
2025-09-03 13:10:47 +0530

    push

--- day 2-opreters/log-consol.js
+++ day 2-opreters/log-consol.js
@@ -3,3 +3,4 @@
         console.info("I am 23 years old");
         console.error("I am from Navsari");
         console.info("I am a student of full stack web development");
+        console.info("I am a student of full stack web development");
